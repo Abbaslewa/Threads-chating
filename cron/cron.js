33967383aa83e318b3470f376db5d43e199a2e97
AbbasLewa <abbasomerhassn@gmail.com>
2025-03-14 08:48:46 +0200

    chating

--- cron/cron.js
+++ cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "http://localhost:3000";
+const URL = "https://threads-forntend-backend.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
